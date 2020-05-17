051df78c1016e5e76566bce328518a38cf580b8a
Ryan Johnston <RyanoJohnston@hotmail.com>
2020-05-17 20:23:56 +0100

    Initial Commit

---
+++ Kode4Kids/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kode4Kids
+//
+//  Created by Ryan Johnston on 17/05/2020.
+//  Copyright © 2020 Group9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Kode4KidsTests/Kode4KidsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Kode4KidsTests.swift
+//  Kode4KidsTests
+//
+//  Created by Ryan Johnston on 17/05/2020.
+//  Copyright © 2020 Group9. All rights reserved.
+//
+
+import XCTest
+@testable import Kode4Kids
+
+class Kode4KidsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
