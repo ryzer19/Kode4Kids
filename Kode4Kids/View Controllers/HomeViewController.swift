8a7c1dcb5b8985b8121b2d2f2ccb836308735541
Caleb Clegg <x17367481@student.ncirl.ie>
2020-06-15 03:31:17 +0100

    removed all navigation functionality to just a blank page for home

--- Kode4Kids/View Controllers/HomeViewController.swift
+++ Kode4Kids/View Controllers/HomeViewController.swift
@@ -10,10 +10,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet weak var leading: NSLayoutConstraint!
-    
-    @IBOutlet weak var trailing: NSLayoutConstraint!
-    
+  
     var menuOut = false
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,28 +30,6 @@ class HomeViewController: UIViewController {
     */
     
     
-    @IBAction func menuTapped(_ sender: Any) {
-        
-        if menuOut == false {
-            
-            leading.constant = 150
-            trailing.constant = -150
-            
-            menuOut = true
-        } else {
-            leading.constant = 0
-            trailing.constant = 0
-            
-            menuOut = false
-            
-        }
-        
-       UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations:{ self.view.layoutIfNeeded() })
-              {(animationComplete) in
-                  print("The animation is complete")
-                  
-              
-              }
-    }
+    
     
 }
