66d1248da13a4532381fdf337b581a069c7eefbd
1Bibz <omotope16@gmail.com>
2020-07-05 01:14:27 +0100

    Forum Page improvements

--- Kode4Kids/View Controllers/Models/Post.swift
+++ Kode4Kids/View Controllers/Models/Post.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+
+class Post{
+    var id:String
+    var author:String
+    var text:String
+    
+    init(id:String, author:String, text:String) {
+        self.id = id
+        self.author = author
+        self.text = text
+    }
+}
